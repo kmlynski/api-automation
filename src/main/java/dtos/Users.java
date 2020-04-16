a04280260fdae838c396b13e71cff6c2d4314f84
Kamil <kamil.mlynski@lottomart.com>
2020-04-16 10:48:23 +0200

    added users simple dto

---
+++ src/main/java/dtos/Users.java
@@ -0,0 +1,15 @@
+package dtos;
+
+public class Users {
+
+    private String login;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
