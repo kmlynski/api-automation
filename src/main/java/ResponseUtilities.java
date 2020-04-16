62a5ceb12a965ba85b7ba61e60853d83f1abbcf3
Kamil <kamil.mlynski@lottomart.com>
2020-04-16 10:20:31 +0200

    added generic method for json object creation

--- src/main/java/ResponseUtilities.java
+++ src/main/java/ResponseUtilities.java
@@ -1,25 +1,38 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class ResponseUtilities {
+class ResponseUtilities {
 
-    public static String getHeader(CloseableHttpResponse response, String headerName) {
+    static String getHeader(CloseableHttpResponse response, String headerName) {
         List<Header> httpHeaders = Arrays.asList(response.getAllHeaders());
 
         Header matchedHeader = httpHeaders.stream()
-                                .filter(header -> headerName.equalsIgnoreCase(header.getName()))
-                                .findFirst().orElseThrow(() -> new RuntimeException("Didn't finde the header"));
+                .filter(header -> headerName.equalsIgnoreCase(header.getName()))
+                .findFirst().orElseThrow(() -> new RuntimeException("Didn't finde the header"));
         return matchedHeader.getValue();
     }
 
-    public static boolean isHeaderPresent(CloseableHttpResponse response, final String headerName){
+    static boolean isHeaderPresent(CloseableHttpResponse response, final String headerName) {
         Header[] headers = response.getAllHeaders();
         List<Header> httpHeaders = Arrays.asList(headers);
 
         return httpHeaders.stream()
-                .anyMatch(header  -> header.getName().equalsIgnoreCase(headerName));
+                .anyMatch(header -> header.getName().equalsIgnoreCase(headerName));
+    }
+
+    static <T> T unmarshall(CloseableHttpResponse response, Class<T> _class) throws IOException {
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .readValue(jsonBody, _class);
+
     }
 }
