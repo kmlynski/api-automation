20e3d6f6129e23a69bfe538f60abb946e0e107b6
Kamil <kamil.mlynski@lottomart.com>
2020-04-16 10:56:04 +0200

    added ability to read from system variables/config file

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,37 @@
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.FileReader;
 import java.io.IOException;
 
 public class BaseTest {
-    public static final String BASE_URL = "https://api.github.com";
-    public CloseableHttpClient client;
-    public CloseableHttpResponse response;
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+    static String TEST_USER;
+    static String BASE_URL;
+
+    @BeforeSuite
+    @SuppressWarnings("checked")
+    public void setUp() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/config.conf.json"));
+
+        TEST_USER = System.getenv("TEST_USER");
+        if (TEST_USER == null) {
+            TEST_USER = (String) config.get("testusername");
+        }
+
+        String server = System.getenv("SERVER");
+        if(server == null){
+            BASE_URL = (String) config.get("server");
+        }
+    }
 
     @BeforeMethod
     public void SetUp(){
